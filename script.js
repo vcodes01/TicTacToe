1fdc50528b2deacdd9069d90b57b2606a6fa52c9
Victoria <vickymlynch@gmail.com>
2022-01-04 04:08:05 -0500

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// The forEach() method calls a function for each element in an array.
+const cellElements = document.querySelectorAll('[data-cell]')
+callElements.foreach(cell => {
+    cell.addEventListener ('click', handleClick {once: true})
+})
+function handleClick(e) {
+    
+}
